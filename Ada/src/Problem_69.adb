c807f6411a375b412320be068b8c6215d05b4964
Tim Bollman <Tim.Bollman@gmail.com>
2016-07-16 08:46:20 -0700

    Add comment as to why the multiply smallest primes works for that problem that I thought through on my run yesterday morning.

--- Ada/src/Problem_69.adb
+++ Ada/src/Problem_69.adb
@@ -10,6 +10,15 @@ package body Problem_69 is
       prime : One_Million;
       gen : Million_Primes.Prime_Generator := Million_Primes.Make_Generator(One_Million'Last);
    begin
+      -- The Euler number can be calculated by multiplying together the percent of the numbers that
+      -- each prime factor of the number will remove.  Since we're scaling by the number itself
+      -- getting bigger numbers will not help us, just a larger percent of numbers filtered.  Each
+      -- prime factor will filter out 1/p of the numbers in the number line, leaving p-1/p
+      -- remaining. So the smaller the primes we can pick, the larger the percent of numbers we will
+      -- filter out. And since we're picking the smallest primes, we know we could never pick more
+      -- larger primes and end up with a smaller end target (because we could replace the larger
+      -- prime with a smaller prime, get a better ratio). As a result we can just multiply from the
+      -- smallest prime number upwards until we get above our maximum ratio.
       loop
          Million_Primes.Next_Prime(gen, prime);
          exit when prime = 1;
